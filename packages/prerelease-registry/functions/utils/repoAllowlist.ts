028d1019c83cb3f18bed085c4a366d2a61248445
James <james@eli.cx>
2024-10-01 23:05:04 +0100

    new project

--- packages/prerelease-registry/functions/utils/repoAllowlist.ts
+++ packages/prerelease-registry/functions/utils/repoAllowlist.ts
@@ -1,5 +1,8 @@
+export const account = "james-elicx";
+
 export const repos: [string, string][] = [
 	["james-elicx", "itty-fs-router"],
 	["james-elicx", "next-on-pages"],
 	["james-elicx", "cf-bindings-proxy"],
+	["james-elicx", "build-output-router"],
 ];
